4518784d3f9160b413a0e81c3f923aa050499432
Steve Schrab <megasmack@users.noreply.github.com>
2026-03-25 10:55:06 -0500

    Update Header styles and add Custom tab

--- force-app/main/default/lwc/smsStyleGuide/smsStyleGuide.js
+++ force-app/main/default/lwc/smsStyleGuide/smsStyleGuide.js
@@ -1,33 +1,57 @@
-import { LightningElement, wire } from 'lwc';
-import { CurrentPageReference } from 'lightning/navigation';
-import Toast from 'lightning/toast';
-import ToastContainer from 'lightning/toastContainer';
-import AccordionPage from './_accordion.html';
-import ButtonsPage from './_buttons.html';
-import ColorsPage from './_colors.html';
-import FormPage from './_form-fields.html';
-import ToastsPage from './_toasts.html';
-import TabsPage from './_tabs.html';
-import TypographyPage from './_typography.html';
-import { COLUMN_SIZE_CLASSES, DXP_COLORS, NUM_OF_TABS } from './constants';
+/**
+ * Created by Steve M Schrab
+ * https://github.com/megasmack/lwr-style-guide
+ */
 
+import { LightningElement, api, wire } from "lwc";
+import { CurrentPageReference } from "lightning/navigation";
+import Toast from "lightning/toast";
+import ToastContainer from "lightning/toastContainer";
+import AccordionPage from "./_accordion.html";
+import ButtonsPage from "./_buttons.html";
+import ColorsPage from "./_colors.html";
+import FormPage from "./_form-fields.html";
+import ToastsPage from "./_toasts.html";
+import TabsPage from "./_tabs.html";
+import TypographyPage from "./_typography.html";
+import CustomPage from "./_slot.html";
+import { COLUMN_SIZE_CLASSES, DXP_COLORS, NUM_OF_TABS } from "./constants";
+
+/**
+ * @slot CustomTitle
+ * @slot CustomContent
+ */
 export default class SmsStyleGuide extends LightningElement {
+  // --- Public Properties ---
+
+  @api startingPage = "accordion";
+  @api addCustom = false;
 
   // --- Private Properties ---
 
   styleHooks = [];
-  currentPage = 'buttons';
   pagesData = [
-    { label: 'Buttons', value: 'buttons' },
-    { label: 'Colors', value: 'colors' },
-    { label: 'Form Fields', value: 'form' },
-    { label: 'Tabs', value: 'tabs' },
-    { label: 'Toasts', value: 'toasts' },
-    { label: 'Typography', value: 'typography' },
-    { label: 'Accordion', value: 'accordion' },
+    { label: "Accordion", value: "accordion" },
+    { label: "Buttons", value: "buttons" },
+    { label: "Colors", value: "colors" },
+    { label: "Form Fields", value: "form" },
+    { label: "Tabs", value: "tabs" },
+    { label: "Toasts", value: "toasts" },
+    { label: "Typography", value: "typography" },
+    { label: "Custom", value: "custom" }
   ];
+  currentPage;
   colorsMapped = false;
   publishedState;
+
+  radioValue = "option1";
+
+  radioOptions = [
+    { label: "Sales", value: "option1" },
+    { label: "Force", value: "option2" }
+  ];
+
+  typographySample = "The quick brown fox jumps over the lazy dog.";
 
   // --- Wire Methods ---
 
@@ -36,10 +60,10 @@ export default class SmsStyleGuide extends LightningElement {
     const app = currentPageReference?.state?.app;
     const { hash } = window.location;
     const page = hash?.slice(1);
-    if (app === 'commeditor') {
-      this.publishedState = 'Draft';
+    if (app === "commeditor") {
+      this.publishedState = "Draft";
     } else {
-      this.publishedState = 'Live';
+      this.publishedState = "Live";
     }
     if (page && this.pagesData.some((h) => h.value === page)) {
       this.currentPage = page;
@@ -49,32 +73,35 @@ export default class SmsStyleGuide extends LightningElement {
   // --- Lifecycle Hooks ---
 
   connectedCallback() {
+    this.currentPage = this.startingPage;
     const toastContainer = ToastContainer.instance();
     toastContainer.maxToasts = 5;
-    toastContainer.toastPosition = 'top-right';
+    toastContainer.toastPosition = "top-right";
   }
 
   render() {
     switch (this.currentPage) {
-      case 'accordion':
+      case "accordion":
         return AccordionPage;
-      case 'colors':
+      case "colors":
         return ColorsPage;
-      case 'form':
+      case "form":
         return FormPage;
-      case 'toasts':
+      case "toasts":
         return ToastsPage;
-      case 'tabs':
+      case "tabs":
         return TabsPage;
-      case 'typography':
+      case "typography":
         return TypographyPage;
+      case "custom":
+        return CustomPage;
       default:
         return ButtonsPage;
     }
   }
 
   renderedCallback() {
-    if (this.currentPage === 'colors' && !this.colorsMapped) {
+    if (this.currentPage === "colors" && !this.colorsMapped) {
       this.styleHooks = this.mapColors(DXP_COLORS);
       this.colorsMapped = true;
     } else {
@@ -92,7 +119,8 @@ export default class SmsStyleGuide extends LightningElement {
   mapColors(colors) {
     return colors.map((section, idx) => {
       const cssVarLength = section.cssVars.length;
-      const cssVarIndex = cssVarLength / 2 >= 1 ? cssVarLength / 2 : cssVarLength;
+      const cssVarIndex =
+        cssVarLength / 2 >= 1 ? cssVarLength / 2 : cssVarLength;
       const cssGroup1 = section.cssVars.slice(0, cssVarIndex);
       const cssGroup2 = section.cssVars.slice(cssVarIndex, cssVarLength);
       const cssGroups = [cssGroup1, cssGroup2];
@@ -105,8 +133,8 @@ export default class SmsStyleGuide extends LightningElement {
             return {
               key: `sms-style-guide_color-${i}`,
               cssVar,
-              hexColor: color?.startsWith('#') ? color : this.rgbToHex(color),
-              rgbColor: color?.startsWith('rgb') ? color : this.hexToRgb(color),
+              hexColor: color?.startsWith("#") ? color : this.rgbToHex(color),
+              rgbColor: color?.startsWith("rgb") ? color : this.hexToRgb(color),
               classes: COLUMN_SIZE_CLASSES,
               style: color
                 ? `--sms-style-guide_color: var(${cssVar})`
@@ -124,12 +152,11 @@ export default class SmsStyleGuide extends LightningElement {
    * @returns {string}
    */
   getColor(cssVar) {
-    // eslint-disable-next-line @lwc/lwc/no-document-query
     const el = this.refs.colors;
     if (el && cssVar) {
       return getComputedStyle(el).getPropertyValue(cssVar);
     }
-    return '';
+    return "";
   }
 
   /**
@@ -148,7 +175,7 @@ export default class SmsStyleGuide extends LightningElement {
     const r = parseInt(result[1], 16);
     const g = parseInt(result[2], 16);
     const b = parseInt(result[3], 16);
-    return result ? `rgb(${r}, ${g}, ${b})` : '';
+    return result ? `rgb(${r}, ${g}, ${b})` : "";
   }
 
   /**
@@ -167,15 +194,15 @@ export default class SmsStyleGuide extends LightningElement {
       if (a) {
         // TODO: Handle alpha channel
         return (
-          '#' + ((1 << 24) | (r << 16) | (g << 8) | b).toString(16).slice(1)
+          "#" + ((1 << 24) | (r << 16) | (g << 8) | b).toString(16).slice(1)
         );
       }
-      return '#' + ((1 << 24) | (r << 16) | (g << 8) | b).toString(16).slice(1);
+      return "#" + ((1 << 24) | (r << 16) | (g << 8) | b).toString(16).slice(1);
     }
-    return '';
+    return "";
   }
 
-  showToast(label, message, variant = 'info', mode = 'sticky') {
+  showToast(label, message, variant = "info", mode = "sticky") {
     Toast.show(
       {
         label,
@@ -194,15 +221,16 @@ export default class SmsStyleGuide extends LightningElement {
     return tabArray.map((_t, i) => ({
       value: `${i}`,
       label: `Item ${i}`,
-      content: `Tab Content ${i}`,
+      content: `Tab Content ${i}`
     }));
   }
 
   get pages() {
-    const baseClass = 'slds-tabs_default__item';
-    const activeClass = 'slds-is-active';
-    return this.pagesData.map((page) => {
-      return {
+    const baseClass = "slds-tabs_default__item";
+    const activeClass = "slds-is-active";
+    const hasCustom = this.addCustom || this.startingPage === "custom";
+    return this.pagesData
+      .map((page) => ({
         ...page,
         classes:
           page.value === this.currentPage
@@ -210,12 +238,12 @@ export default class SmsStyleGuide extends LightningElement {
             : baseClass,
         active: page.value === this.currentPage,
         tabIndex: page.value === this.currentPage ? -1 : 0
-      };
-    });
+      }))
+      .filter((page) => !(page.value === "custom" && !hasCustom));
   }
 
   get isLive() {
-    return this.publishedState === 'Live';
+    return this.publishedState === "Live";
   }
 
   // --- Event Handlers ---
@@ -227,10 +255,10 @@ export default class SmsStyleGuide extends LightningElement {
       navigator.clipboard
         .writeText(code)
         .then(() => {
-          this.showToast('Copied to Clipboard', '', 'success', 'pester');
+          this.showToast("Copied to Clipboard", "", "success", "pester");
         })
         .catch((error) => {
-          console.error('Failed to copy', error);
+          console.error("Failed to copy", error);
         });
     } else if (!this.isLive) {
       // The iframe in builder does not give us access to the clipboard.
@@ -241,7 +269,7 @@ export default class SmsStyleGuide extends LightningElement {
       selection.removeAllRanges();
       selection.addRange(range);
     } else {
-      console.error('Failed to copy');
+      console.error("Failed to copy");
     }
   }
 
